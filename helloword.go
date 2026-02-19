279f784fafcd7113363a6f43c68d33faae8ffb11
baccend12dev <karyadisimamora0105@gmail.com>
2026-02-19 09:43:15 +0700

    add comment

--- helloword.go
+++ helloword.go
@@ -1,7 +1,10 @@
 package main
 
+//import "fmt"
 import "fmt"
 
+// using fmt package to print Hello, World!! to the console
+// main function is the entry point of the program
 func main() {
 	fmt.Println("Hello, World!!")
 }
